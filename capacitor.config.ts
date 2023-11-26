62144ef8ccca0c570e84e705780dea13389bea72
DenisseVM <denisse.vmunoz@gmail.com>
2023-11-26 10:47:41 -0300

    -
    -

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'ImHere',
+  appName: 'AsissApp',
   webDir: 'www',
   bundledWebRuntime: false
 };
